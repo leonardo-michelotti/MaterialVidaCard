2bc53395e77f026799a5764f23452c643828e6e1
leonardo.michelotti <leomsn32@gmail.com>
2024-11-27 01:23:15 -0300

    Pré Deploy

--- MaterialVidaCard/marketing/views.py
+++ MaterialVidaCard/marketing/views.py
@@ -1,8 +1,6 @@
-# marketing/views.py
-
 from django.contrib.auth.decorators import login_required
-from django.http import FileResponse, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 from .drive_service import get_drive_service
 
@@ -13,7 +11,7 @@ def list_files_in_folder(service, folder_id):
         fields="files(id, name, webViewLink, webContentLink)").execute()
     return results.get('files', [])
 
-
+@login_required
 def marketing_index(request):
     service = get_drive_service()
 
@@ -21,12 +19,29 @@ def marketing_index(request):
     campanhas_folder_id = '1awW6ZIfcohpuPHIrNI5yoFblsq1f7PFr'
     material_apoio_folder_id = '1x7bGV_2bHUP0KFCs1Nk1SnJA-QaO0a08'
 
+    # Fetching files from Google Drive folders
     campanhas = list_files_in_folder(service, campanhas_folder_id)
     materiais_apoio = list_files_in_folder(service, material_apoio_folder_id)
+
+    # Process selected campaign
+    selected_campaign_id = request.GET.get('selected_campaign')
+    selected_campaign = None
+    if selected_campaign_id:
+        selected_campaign = next((file for file in campanhas if file['id'] == selected_campaign_id), None)
+
+    # Process selected material of support
+    selected_material_id = request.GET.get('selected_material')
+    selected_material = None
+    if selected_material_id:
+        selected_material = next((file for file in materiais_apoio if file['id'] == selected_material_id), None)
 
     return render(request, 'marketing/index.html', {
         'campanhas': campanhas,
         'materiais_apoio': materiais_apoio,
+        'selected_campaign': selected_campaign,
+        'selected_campaign_id': selected_campaign_id,
+        'selected_material': selected_material,
+        'selected_material_id': selected_material_id,
     })
 
 
